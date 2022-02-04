a967a3ff2f86690516d05ad3614f370b3b6e74e4
Dima Yanovskji <86802309+Defisto7@users.noreply.github.com>
2022-02-04 20:20:20 +0200

    master
    add spinner

---
+++ src/components/spinner/spinner.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './spinner.css'
+
+const Spinner = () => {
+    return (
+        <div class="loadingio-spinner-spinner-c8u2spqaeot">
+            <div class="ldio-ct96jao7eav">
+        <div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div>
+        </div></div>
+        )
+}
+
+export default Spinner;
+
+
